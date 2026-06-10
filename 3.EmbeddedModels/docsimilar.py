7fc8e7d0b10b4e05108200f8bbb3861cae75c0ba
Hassan <maskedw248@gmail.com>
2026-06-10 17:55:45 +0500

    Made a semantic search application

--- 3.EmbeddedModels/docsimilar.py
+++ 3.EmbeddedModels/docsimilar.py
@@ -29,49 +29,85 @@
 # print("Similarity score is ", score)
 
 
+# from langchain_google_genai import GoogleGenerativeAIEmbeddings
+# from dotenv import load_dotenv
+# from sklearn.metrics.pairwise import cosine_similarity
+# import numpy as np
+# load_dotenv()
+# model = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001", dimensions=200)
+# docuemnt = [
+# "Virat Kohli is an Indian cricketer known for his aggressive batting and leadership.",
+
+# "MS Dhoni is a former Indian captain famous for his calm demeanor and finishing skills.",
+
+# "Sachin Tendulkar, also known as the 'God of Cricket', holds many batting records.",
+
+# "Rohit Sharma is known for his elegant batting and record-breaking double centuries.",
+
+# "Jasprit Bumrah is an Indian fast bowler known for his unorthodox action and yorkers.",
+
+# ]
+# query = "tell me about sachin"
 
 
+# from langchain_google_genai import GoogleGenerativeAIEmbeddings
+# from dotenv import load_dotenv
+# import numpy as np
+# from sklearn.metrics.pairwise import cosine_similarity
+# load_dotenv()
+# Embded = GoogleGenerativeAIEmbeddings(model = "models/gemini-embedding-001", dimensions = 32)
 
+# docs = [
+#     "Virat Kohli is an Indian cricketer known for his aggressive batting and leadership.",
 
+#  "MS Dhoni is a former Indian captain famous for his calm demeanor and finishing skills.",
 
+#  "Sachin Tendulkar, also known as the 'God of Cricket', holds many batting records.",
 
+#  "Rohit Sharma is known for his elegant batting and record-breaking double centuries.",
 
+# "Jasprit Bumrah is an Indian fast bowler known for his unorthodox action and yorkers.",
 
+# ]
 
+# question = "Who is Rohit sharma"
 
-
-
+# user_input = Embded.embed_query(question)
+# Document = Embded.embed_documents(docs)
+# similarity = cosine_similarity([user_input],Document)[0]
+# output =sorted(list(enumerate(similarity)),key = lambda x:x[1] )
+# index, score = output[-1]
+# print(question)
+# print(docs[index])
+# print (score)
 
 
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from dotenv import load_dotenv
-import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 load_dotenv()
-Embded = GoogleGenerativeAIEmbeddings(model = "models/gemini-embedding-001", dimensions = 32)
+model = GoogleGenerativeAIEmbeddings(model = "models/gemini-embedding-001",dimensions=200)
+docuemnt = [
+"Virat Kohli is an Indian cricketer known for his aggressive batting and leadership.",
 
-docs = [
-    "Virat Kohli is an Indian cricketer known for his aggressive batting and leadership.",
+"MS Dhoni is a former Indian captain famous for his calm demeanor and finishing skills.",
 
- "MS Dhoni is a former Indian captain famous for his calm demeanor and finishing skills.",
+"Sachin Tendulkar, also known as the 'God of Cricket', holds many batting records.",
 
- "Sachin Tendulkar, also known as the 'God of Cricket', holds many batting records.",
-
- "Rohit Sharma is known for his elegant batting and record-breaking double centuries.",
+"Rohit Sharma is known for his elegant batting and record-breaking double centuries.",
 
 "Jasprit Bumrah is an Indian fast bowler known for his unorthodox action and yorkers.",
 
 ]
-
-question = "Who is Rohit sharma"
-
-user_input = Embded.embed_query(question)
-Document = Embded.embed_documents(docs)
-similarity = cosine_similarity([user_input],Document)[0]
-output =sorted(list(enumerate(similarity)),key = lambda x:x[1] )
-index, score = output[-1]
-print(question)
-print(docs[index])
-print (score)
-
-
+query = "tell me about sachin"
+doc_embedding = model.embed_documents(docuemnt)
+query_embed = model.embed_query(query)
+score = cosine_similarity([query_embed] , doc_embedding)[0]
+sorted_score = sorted(list(enumerate(score)),key = lambda x:x[1])
+print("sorted score", sorted_score)
+best_match = sorted_score[-1]
+index = best_match[0]
+similar = best_match[1]
+print(query)
+print(docuemnt[index])
+print(score)
