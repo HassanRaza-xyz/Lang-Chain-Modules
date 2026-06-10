8129324ae1a3155215ac8627ec725168ad258d21
Hassan <maskedw248@gmail.com>
2026-06-10 15:56:21 +0500

    Downlaoded Local model of 384 dimensions

---
+++ 3.EmbeddedModels/hf.py
@@ -0,0 +1,5 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+embed = HuggingFaceEmbeddings(model = "sentence-transformers/all-MiniLM-L6-v2")
+text = "My name is Hassan"
+output = embed.embed_query(text)
+print(len(output))
