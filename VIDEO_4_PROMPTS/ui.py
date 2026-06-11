971b51e80f48c446715178c82b06f9b9013dd5cc
Hassan <maskedw248@gmail.com>
2026-06-11 22:54:54 +0500

    Make a research assistant using dynamic prompt

--- VIDEO_4_PROMPTS/ui.py
+++ VIDEO_4_PROMPTS/ui.py
@@ -3,14 +3,14 @@ from dotenv import load_dotenv
 import streamlit as st
 load_dotenv()
 from langchain_core.prompts import PromptTemplate
-model = ChatGoogleGenerativeAI(model = "gemini-2.5-flash")
+model = ChatGoogleGenerativeAI(model = "gemini-2.5-flash", temperature = 0.5)
 st.header("Research Input")
 paper_input = st.selectbox( "Select Research Paper Name", ["Select...", "Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
 
 style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] )
 
 length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
-
+user_input = st.text_area("Any specific instructions?")
 template = PromptTemplate(
     template="""   
     Please summarize the research paper titled "{paper_input}" with the following specifications:
@@ -22,16 +22,17 @@ Explanation Length: {length_input}
 2. Analogies:
     - Use relatable analogies to simplify complex ideas.
 If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.
-Ensure the summary is clear, accurate, and aligned with the provided style and length.""",
-input_variables=['paper_input','style_input','length_input']
+Ensure the summary is clear, accurate, and aligned with the provided style and length.
+{user_input}""",
+input_variables=['paper_input','style_input','length_input','user_input']
 )
-user_input = st.chat_input("Want to add more details?")
 prompt = template.invoke({
     'paper_input': paper_input,
     'style_input' : style_input,
     'length_input': length_input,
+    'user_input': user_input,
 })
 
 if st.button("Summarize"):
-               result = model.invoke(prompt,user_input)
+               result = model.invoke(prompt)
                st.write(result.content)
