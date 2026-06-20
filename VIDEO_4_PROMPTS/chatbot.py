3c73242e0134ab4dc100df2b2747dd2d66c31ddc
Hassan <maskedw248@gmail.com>
2026-06-20 22:04:30 +0500

    Created Chat history

--- VIDEO_4_PROMPTS/chatbot.py
+++ VIDEO_4_PROMPTS/chatbot.py
@@ -1,11 +1,13 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 load_dotenv()
+chat_history = []
 model = ChatGoogleGenerativeAI(model = "gemini-2.5-flash")
 while True:
+    chat_history.append(user_input)
     user_input = input("You")
     if user_input == "exit":
         break
-    result =  model.invoke(user_input)
+    result =  model.invoke(chat_history)
     print("Ai",result.content)
     
